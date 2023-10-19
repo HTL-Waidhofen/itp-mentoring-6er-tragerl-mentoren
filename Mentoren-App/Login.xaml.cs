3dabbd8688675a0702278c74db0b24d6729cd03f
killian.mayrhofer <killian.mayrhofer@htlwy.at>
2023-10-19 08:36:49 +0200

    Login.xaml.cs LogUserIn SQL Code

--- Mentoren-App/Login.xaml.cs
+++ Mentoren-App/Login.xaml.cs
@@ -24,5 +24,49 @@ namespace Mentoren_App
         {
             InitializeComponent();
         }
+
+        private void LogUserIn(object sender, RoutedEventArgs e)
+        {
+            //Methode für den Login mit Email/ Passwort# mit rückgabe von true/false und ist User/Admin/Mentor
+            //Wenn true => Redirect auf Page
+
+            SqlConnection sqlCon = new SqlConnection(@"");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tblUser WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void UserRegistration(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
     }
 }
