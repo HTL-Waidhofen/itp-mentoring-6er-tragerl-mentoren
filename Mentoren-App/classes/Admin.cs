8e41bc97dd3300cf96e5f30fb7539c956bb9875c
HeschlMaximilian <144880339+HeschlMaximilian@users.noreply.github.com>
2023-10-19 08:39:16 +0200

    Made Classes function

--- Mentoren-App/classes/Admin.cs
+++ Mentoren-App/classes/Admin.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 public class Admin : Base
 {
 	public Admin()
 	{
-		role = "admin";
+		Role = "admin";
 	}
 }
--- Mentoren-App/classes/Baseclass.cs
+++ Mentoren-App/classes/Baseclass.cs
@@ -1,17 +1,29 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 public abstract class Base
 {
-	int ID { get; }
-	string FName { get; }
-	string Name { get; }
-	string Role { get; set; }
-	string Password { get; set; }
-    string Email { get; set; }
+	public int ID { get; }
+	public string FName { get; }
+    public string Name { get; }
+	public string Role { get; set; }
+	public string Password { get; set; }
+    public string Email { get; set; }
 	public Base()
 	{
 	}
-	public bool LoginSucess(string email, string password, list<Base> list)
+	public bool LoginSucess(string email, string password, List<Base> list)
 	{
         foreach (Base user in list)
         {
@@ -19,7 +31,7 @@ public abstract class Base
             {
                 if (password == user.Password) 
                 {
-                        return true                 
+                    return true;                 
                 }
             }
         }
