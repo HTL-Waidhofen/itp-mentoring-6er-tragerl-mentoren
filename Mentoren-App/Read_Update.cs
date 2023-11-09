053581425f2333a2b63bf71431853b4879684890
Pop-Ana <ana.pop@htlwy.at>
2023-11-09 08:22:52 +0100

    fixed typo mistake

--- Mentoren-App/Read_Update.cs
+++ Mentoren-App/Read_Update.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace Mentoren_App
 {
-<<<<<<< HEAD
     public class Read_Update
-=======
-    internal class Read_Update
->>>>>>> 5d70ceb (created the class Read_Update)
     {
+
     }
+
 }
