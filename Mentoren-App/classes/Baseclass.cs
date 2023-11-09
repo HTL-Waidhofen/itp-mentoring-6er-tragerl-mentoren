eab2c4c57ad3c4d109063af0bb2dd31cd173990b
Help has been denied <jakob.hintsteiner@htlwy.at>
2023-11-09 09:35:57 +0100

    Baseclass macht faxxen

--- Mentoren-App/classes/Baseclass.cs
+++ Mentoren-App/classes/Baseclass.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.collections;
 public class User
 {
 	public int ID { get; }
