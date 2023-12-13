38360d1714be1de80a2c9657952e4efb252a5f85
MayrhoferKillian <113448701+MayrhoferKillian@users.noreply.github.com>
2023-12-13 17:11:28 +0100

    Update DbConnection.cs

--- Mentoren-App/classes/DbConnection.cs
+++ Mentoren-App/classes/DbConnection.cs
@@ -1,7 +1,74 @@
-﻿using Microsoft.Identity.Client;
 using System;
+using System.Data.SqlClient;
 
 public abstract class DbConnection
 {
-    public string ConnectionString = "";
+    public string ConnectionString = "Dein_Datenbank_Connection_String";
+
+    // CREATE: Allgemeine Methode zum Hinzufügen von Daten
+    public void CreateData(string query, SqlParameter[] parameters)
+    {
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // READ: Allgemeine Methode zum Lesen von Daten
+    public SqlDataReader ReadData(string query, SqlParameter[] parameters)
+    {
+        SqlConnection connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        using (SqlCommand cmd = new SqlCommand(query, connection))
+        {
+            if (parameters != null)
+            {
+                cmd.Parameters.AddRange(parameters);
+            }
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+    }
+
+    // UPDATE: Allgemeine Methode zum Aktualisieren von Daten
+    public void UpdateData(string query, SqlParameter[] parameters)
+    {
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // DELETE: Allgemeine Methode zum Löschen von Daten
+    public void DeleteData(string query, SqlParameter[] parameters)
+    {
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    cmd.Parameters.AddRange(parameters);
+                }
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
 }
