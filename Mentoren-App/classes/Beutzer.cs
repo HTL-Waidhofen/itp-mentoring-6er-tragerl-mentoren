0b5a9e50be8c57d037ee7d46921b0e824759c3d2
MayrhoferKillian <113448701+MayrhoferKillian@users.noreply.github.com>
2023-12-13 17:09:11 +0100

    Update Beutzer.cs

--- Mentoren-App/classes/Beutzer.cs
+++ Mentoren-App/classes/Beutzer.cs
@@ -1,31 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections;
+using System.Data.SqlClient;
+
 public class Benutzer : DbConnection
 {
-	public int ID { get; }
-	public string Vorname { get; }
+    public int ID { get; }
+    public string Vorname { get; }
     public string Nachname { get; }
-	public string Role { get; set; }
-	public string Email { get; set; }
+    public string Role { get; set; }
+    public string Email { get; set; }
     private string Passwort { get; set; }
+
     public override string ToString()
     {
         return Vorname + " " + Nachname + ", Email: " + Email;
     }
-    Benutzer(int id, string vorname, string nachname, string role, string email, string passwort) 
+
+    public Benutzer(int id, string vorname, string nachname, string role, string email, string passwort)
     {
         this.ID = id;
         this.Vorname = vorname;
@@ -34,6 +25,84 @@ public class Benutzer : DbConnection
         this.Email = email;
         this.Passwort = passwort;
     }
-}
 
+    // CREATE: Neuen Benutzer hinzufügen
+    public static void CreateBenutzer(string vorname, string nachname, string role, string email, string passwort)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Benutzer (Vorname, Nachname, Rolle, Email, Passwort) VALUES (@Vorname, @Nachname, @Role, @Email, @Passwort)";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@Vorname", vorname);
+                cmd.Parameters.AddWithValue("@Nachname", nachname);
+                cmd.Parameters.AddWithValue("@Role", role);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Passwort", passwort);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // READ: Benutzer nach ID abrufen
+    public static Benutzer ReadBenutzerByID(int id)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM Benutzer WHERE ID = @ID";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@ID", id);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Benutzer(
+                            Convert.ToInt32(reader["ID"]),
+                            Convert.ToString(reader["Vorname"]),
+                            Convert.ToString(reader["Nachname"]),
+                            Convert.ToString(reader["Rolle"]),
+                            Convert.ToString(reader["Email"]),
+                            Convert.ToString(reader["Passwort"])
+                        );
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    // UPDATE: E-Mail-Adresse eines Benutzers aktualisieren
+    public void UpdateBenutzerEmail(string neueEmail)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "UPDATE Benutzer SET Email = @NeueEmail WHERE ID = @ID";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@NeueEmail", neueEmail);
+                cmd.Parameters.AddWithValue("@ID", this.ID);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // DELETE: Benutzer löschen
+    public void DeleteBenutzer()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "DELETE FROM Benutzer WHERE ID = @ID";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@ID", this.ID);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
 
