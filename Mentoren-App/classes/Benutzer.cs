7adeb682d3d4b1b386372638bf8ebb46f724e27e
Killian Mayrhofer <Killian.mayrhofer@AD.HTLWY.LOGODIDACT.NET>
2023-12-14 08:46:29 +0100

    Update Benutzer.cs

--- Mentoren-App/classes/Benutzer.cs
+++ Mentoren-App/classes/Benutzer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Runtime.CompilerServices;
 
 public class Benutzer : DbConnection
 {
@@ -17,7 +16,7 @@ public class Benutzer : DbConnection
         return Vorname + " " + Nachname + ", Email: " + Email;
     }
 
-    public Benutzer(int id, string vorname, string nachname, string role, string email, string passwort) :base() 
+    public Benutzer(int id, string vorname, string nachname, string role, string email, string passwort) : base()
     {
         this.ID = id;
         this.Vorname = vorname;
@@ -29,9 +28,9 @@ public class Benutzer : DbConnection
         this.ConnectionString = base.ConnectionString;
     }
 
-    public static void CreateBenutzer(string vorname, string nachname, string role, string email, string passwort, string connectionString)
+    public static void CreateBenutzer(string vorname, string nachname, string role, string email, string passwort, string ConnectionString)
     {
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
         {
             connection.Open();
             string query = "INSERT INTO Benutzer (Vorname, Nachname, Rolle, Email, Passwort) VALUES (@Vorname, @Nachname, @Role, @Email, @Passwort)";
@@ -47,9 +46,9 @@ public class Benutzer : DbConnection
         }
     }
 
-    public static Benutzer ReadBenutzerByID(int id, string connectionString)
+    public static Benutzer ReadBenutzerByID(int id, string ConnectionString)
     {
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
         {
             connection.Open();
             string query = "SELECT * FROM Benutzer WHERE ID = @ID";
@@ -75,9 +74,9 @@ public class Benutzer : DbConnection
         return null;
     }
 
-    public void UpdateBenutzerEmail(string neueEmail, string connectionString)
+    public void UpdateBenutzerEmail(string neueEmail, string ConnectionString)
     {
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
         {
             connection.Open();
             string query = "UPDATE Benutzer SET Email = @NeueEmail WHERE ID = @ID";
@@ -90,9 +89,9 @@ public class Benutzer : DbConnection
         }
     }
 
-    public void DeleteBenutzer(string connectionString)
+    public void DeleteBenutzer(string ConnectionString)
     {
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
         {
             connection.Open();
             string query = "DELETE FROM Benutzer WHERE ID = @ID";
@@ -102,6 +101,67 @@ public class Benutzer : DbConnection
                 cmd.ExecuteNonQuery();
             }
         }
+    }
+    public static List<Benutzer> GetAllSchueler(string ConnectionString)
+    {
+        List<Benutzer> result = new List<Benutzer>();
+        string query = "SELECT * FROM Benutzer WHERE Role = 'Schüler'";
+
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["ID"]);
+                        string vorname = Convert.ToString(reader["Vorname"]);
+                        string nachname = Convert.ToString(reader["Nachname"]);
+                        string role = Convert.ToString(reader["Role"]);
+                        string email = Convert.ToString(reader["Email"]);
+                        string passwort = Convert.ToString(reader["Passwort"]);
+
+                        result.Add(new Benutzer(id, vorname, nachname, role, email, passwort));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Benutzer> GetAllMentoren(string ConnectionString)
+    {
+        List<Benutzer> result = new List<Benutzer>();
+        string query = "SELECT * FROM Benutzer WHERE Role = 'Mentor'";
+
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["ID"]);
+                        string vorname = Convert.ToString(reader["Vorname"]);
+                        string nachname = Convert.ToString(reader["Nachname"]);
+                        string role = Convert.ToString(reader["Role"]);
+                        string email = Convert.ToString(reader["Email"]);
+                        string passwort = Convert.ToString(reader["Passwort"]);
+
+                        result.Add(new Benutzer(id, vorname, nachname, role, email, passwort));
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
 
