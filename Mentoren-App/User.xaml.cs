056ab2de5ab77593c1830776b31766b2cf1cd137
Pop-Ana <ana.pop@htlwy.at>
2023-12-14 07:52:14 +0100

    treid sending email

--- Mentoren-App/User.xaml.cs
+++ Mentoren-App/User.xaml.cs
@@ -29,37 +29,33 @@ namespace Mentoren_App
 
         private void SendEmail_Click(object sender, RoutedEventArgs e)
         {
-            try { 
+            try {
 
-            string smtpServer = "smtp.gmail.com";
-            int smtpPort = 587;
-            string smtpBenutzername = "";
-            string smtpPasswort = "";
+                using (var smtpClient = new SmtpClient("smtp.gmail.com"))
+                {
+                    smtpClient.Port = 587;
+                    smtpClient.Credentials = new NetworkCredential("anappp73@gmail.com", "anapopmaria");
+                    smtpClient.EnableSsl = true;
 
-            string absenderEmail = LoginMail_TB.Text;
-            string empfaengerEmail = "";
-            string betreff = Betreff_TB.Text;
-            string text = EmailContent_TB.Text;
+                    using (var mailMessage = new MailMessage())
+                    {
+                        mailMessage.From = new MailAddress("anappp73@gmail.com");
+                        mailMessage.Subject = Betreff_TB.Text;
+                        mailMessage.Body = EmailContent_TB.Text;
+                        mailMessage.IsBodyHtml = true;
+                        mailMessage.To.Add(LoginMail_TB.Text);
 
-            // Erstellen der MailMessage
+                        smtpClient.Send(mailMessage);
+                    }
+                }
 
-            MailMessage mailNachricht = new MailMessage(absenderEmail, empfaengerEmail, betreff, text);
-
-            // Erstellen des SmtpClients und Senden der E-Mail
-
-            SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort);
-            smtpClient.Credentials = new NetworkCredential(smtpBenutzername, smtpPasswort);
-            smtpClient.EnableSsl = true;
-
-            smtpClient.Send(mailNachricht);
-
-            MessageBox.Show("E-Mail wurde erfolgreich gesendet.", "Erfolg", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
+                MessageBox.Show("E-Mail wurde erfolgreich gesendet.", "Erfolg", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
             catch (Exception ex)
             {
                 MessageBox.Show($"Fehler beim Senden der E-Mail: {ex.Message}", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
-}
+        }
     }
 }
