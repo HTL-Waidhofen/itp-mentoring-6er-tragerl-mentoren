539df19c0d88ab4c707558f004e55aa4c991fb59
HeschlMaximilian <144880339+HeschlMaximilian@users.noreply.github.com>
2024-01-23 13:23:18 +0100

    Revert "login function works"

--- Mentoren-App/Login.xaml.cs
+++ Mentoren-App/Login.xaml.cs
@@ -37,24 +37,32 @@ namespace Mentoren_App
 
         private void LogUserIn(object sender, RoutedEventArgs e)
         {
-          
+         
+       
+            //NavigationService?.Navigate(new Uri("User.xaml", UriKind.Relative));
+            NavigationService?.Navigate(new Uri("Admin.xaml", UriKind.Relative));
+            
+            //NavigationService?.Navigate(new Uri("Mentoren.xaml", UriKind.Relative));
+            //Output if Login failed
+            //if(false == true)
+            //MessageBox.Show("Ein Fehler ist aufgetreten. Bitte kontrollieren sie ihre Eingaben. Wenn sie noch nicht registriet sind klicken sie auf 'Registrieren'","Fehler bei Verarbeitung", MessageBoxButton.OK);
+        
+            /*
             MainWindow mainWindow = new MainWindow();
-            foreach (var user in mainWindow.allUsers)
+            foreach (var User in mainWindow.allUsers)
             {
-                string pwd = LoginPwd.Password.ToString();
-                if (user.isLoginDataCorrect(LoginMail.Text, pwd))
-                    mainWindow.currentUser = user;
+                if (User.isLoginDataCorrect(LoginMail.Text, LoginPwd.SecurePassword.ToString()))
+                    mainWindow.currentUser = User;
             }
-            if(mainWindow.currentUser.Role != null) {
-                if(mainWindow.currentUser.Role.Contains('s'))
-                    NavigationService?.Navigate(new Uri("User.xaml", UriKind.Relative));
-                else if (mainWindow.currentUser.Role.Contains('m'))
-                    NavigationService?.Navigate(new Uri("Mentoren.xaml", UriKind.Relative));
-                else if(mainWindow.currentUser.Role.Contains('a'))
-                    NavigationService?.Navigate(new Uri("Admin.xaml", UriKind.Relative));
-            }
+            if(mainWindow.currentUser.Role.Contains('s'))
+                NavigationService?.Navigate(new Uri("User.xaml", UriKind.Relative));
+            else if (mainWindow.currentUser.Role.Contains('m'))
+                NavigationService?.Navigate(new Uri("Mentoren.xaml", UriKind.Relative));
+            else if(mainWindow.currentUser.Role.Contains('a'))
+                NavigationService?.Navigate(new Uri("Admin.xaml", UriKind.Relative));
             else
-                MessageBox.Show("Ein Fehler ist aufggetreten. Bitte kontrollieren sie ihre Eingaben. Wenn sie noch nicht registriert sind klicken sie auf 'Registrieren'", "Fehler bei Verarbeitung", MessageBoxButton.OK);
+                MessageBox.Show("Ein Fehler ist aufgetreten","Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+            */
         }
         public void GoToRegistration(object sender, RoutedEventArgs e)
         {
